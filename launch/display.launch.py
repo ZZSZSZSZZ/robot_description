b41fe2de7b2a945e6fb673c7287d13263d50b50f
ZZSZSZSZZ <52644741+ZZSZSZSZZ@users.noreply.github.com>
2025-08-24 00:52:33 +0800

    first commit

---
+++ launch/display.launch.py
@@ -0,0 +1,64 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+import xacro
+
+def generate_launch_description():
+    package_name = 'robot_description'  # Replace with your package name
+    urdf_file_name = 'robot.xacro'
+    rviz_config_file = 'urdf.rviz'
+
+    rviz_config_path = os.path.join(get_package_share_directory(package_name), 'rviz', rviz_config_file)
+    
+    urdf = os.path.join(
+        get_package_share_directory(package_name),
+        'urdf', "robot",
+        urdf_file_name)
+        
+    doc = xacro.parse(open(urdf))
+    xacro.process_doc(doc)
+   
+    ld = LaunchDescription()
+
+    ld.add_action(DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true'
+    ))
+
+    ld.add_action(Node(
+        package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': doc.toxml()}]
+    ))
+
+    ld.add_action(Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen'
+    ))
+
+    ld.add_action(Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d',rviz_config_path]
+
+    ))
+    
+    ld.add_action(Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_transform_publisher',
+        arguments=['0', '0', '0', '0', '0', '0', 'map', 'base_link'],
+    ))
+
+    return ld
